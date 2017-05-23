758884eb6fba55a82e5eded605d93f81dae99c35
Aditya Arora <arora.aditya520@gmail.com>
2017-05-23 10:52:00 +0530

    Still Incomplete

--- try_2.R
+++ try_2.R
@@ -1,7 +1,8 @@
 rm(list = ls())
-setwd("C:/data/Big-Mart/")
-train = read.csv("Train_UWu5bXk.csv",na.strings = c("","NA"))
-test = read.csv("Test_u94Q5KV.csv",na.strings = c("","NA"))
+#setwd("C:/data/Big-Mart/")
+setwd("D:/Practice/R/AnalyticsVidya/Big Marts Sales 3/")
+train = read.csv("train.csv",na.strings = c("","NA"),stringsAsFactors = F)
+test = read.csv("test.csv",na.strings = c("","NA"),stringsAsFactors = F)
 summary(train)
 summary(test)
 train_sales = train$Item_Outlet_Sales
@@ -37,9 +38,63 @@ test$Item_Weight.x = NULL
 test$Item_Weight = test$Item_Weight.y
 test$Item_Weight.y = NULL
 
-#imputed Size with small using excel
+#imput Size with small (Done analysis using excel)
+train$Outlet_Size[is.na(train$Outlet_Size)] = "Small"
+test$Outlet_Size[is.na(test$Outlet_Size)] = "Small"
+
+
 
 summary(train)
 summary(test)
 
-''
+length(train$Item_Visibility[train$Item_Visibility == 0])
+
+#Feature Engineering
+
+train$Item_Outlet_Sales = train_sales
+Item_quant_per_outlet = train$Item_Outlet_Sales / train$Item_MRP
+
+train$Item_quant_per_outlet = Item_quant_per_outlet
+item_mean = aggregate(train$Item_quant_per_outlet,list(train$Item_Identifier),mean)
+colnames(item_mean) = c("Item_Identifier","Item_mean_sales")
+
+train = merge(train,item_mean,by = "Item_Identifier")
+test = merge(test,item_mean,by = "Item_Identifier",all.x = T)
+summary(train)
+summary(test)
+
+store_mean = aggregate(train$Item_quant_per_outlet,list(train$Outlet_Identifier),mean)
+colnames(store_mean) = c("Outlet_Identifier","Outlet_Mean")
+
+train = merge(train,store_mean,by = "Outlet_Identifier")
+test = merge(test,store_mean,by = "Outlet_Identifier",all.x = T)
+
+submit = read.csv("Sample_sub.csv")
+library(xgboost)
+
+
+train$Item_quant_per_outlet = NULL
+train_sales = train$Item_Outlet_Sales
+train$Item_Outlet_Sales = NULL
+
+train$Outlet_Identifier = NULL
+train$Item_Identifier = NULL
+test$Item_Identifier = NULL
+test$Outlet_Identifier = NULL
+
+train$Item_Fat_Content[train$Item_Fat_Content == "LF"] = 1
+train$Item_Fat_Content[train$Item_Fat_Content == "Low Fat"] = 1
+train$Item_Fat_Content[train$Item_Fat_Content == "low fat"] = 1
+train$Item_Fat_Content[train$Item_Fat_Content == "reg"] = 2
+train$Item_Fat_Content[train$Item_Fat_Content == "Regular"] = 2
+train$Item_Fat_Content = as.integer(train$Item_Fat_Content)
+
+test$Item_Fat_Content[test$Item_Fat_Content == "LF"] = 1
+test$Item_Fat_Content[test$Item_Fat_Content == "Low Fat"] = 1
+test$Item_Fat_Content[test$Item_Fat_Content == "low fat"] = 1
+test$Item_Fat_Content[test$Item_Fat_Content == "reg"] = 2
+test$Item_Fat_Content[test$Item_Fat_Content == "Regular"] = 2
+test$Item_Fat_Content = as.integer(test$Item_Fat_Content)
+
+train$Item_Visibility = as.integer(train$Item_Visibility)
+test$Item_Visibility = as.integer(test$Item_Visibility)
