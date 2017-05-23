2427d67e2dbebbd509c4a1ac1bf9110007701371
Aditya Arora <arora.aditya520@gmail.com>
2017-05-23 11:15:24 +0530

    Still Incomplete

--- try_2.R
+++ try_2.R
@@ -15,12 +15,6 @@ full = rbind(train,test)
 summary(full)
 
 summary(train$Item_Weight)
-library(mice)
-md.pattern(train)
-
-ggplot(train,aes(x = Item_Weight)) + geom_density()
-ggplot(train,aes(x = Item_Type,y = Item_Weight)) + geom_bar(stat = "identity") + 
-          theme(axis.text.x = element_text(angle = 60,hjust = 1))
 
 mean_ident = aggregate(full$Item_Weight,list(full$Item_Identifier),mean,na.rm =T)
 
@@ -98,3 +92,122 @@ test$Item_Fat_Content = as.integer(test$Item_Fat_Content)
 
 train$Item_Visibility = as.integer(train$Item_Visibility)
 test$Item_Visibility = as.integer(test$Item_Visibility)
+
+#Item_type
+train$Item_Type[train$Item_Type == "Baking Goods"] = 1
+test$Item_Type[test$Item_Type == "Baking Goods"] = 1
+
+train$Item_Type[train$Item_Type == "Breads"] = 2
+test$Item_Type[test$Item_Type == "Breads"] = 2
+
+train$Item_Type[train$Item_Type == "Breakfast"] = 3
+test$Item_Type[test$Item_Type == "Breakfast"] = 3
+
+train$Item_Type[train$Item_Type == "Canned"] = 4
+test$Item_Type[test$Item_Type == "Canned"] = 4
+
+train$Item_Type[train$Item_Type == "Dairy"] = 5
+test$Item_Type[test$Item_Type == "Dairy"] = 5
+
+train$Item_Type[train$Item_Type == "Frozen Foods"] = 6
+test$Item_Type[test$Item_Type == "Frozen Foods"] = 6
+
+train$Item_Type[train$Item_Type == "Fruits and Vegetables"] = 7
+test$Item_Type[test$Item_Type == "Fruits and Vegetables"] = 7
+
+train$Item_Type[train$Item_Type == "Hard Drinks"] = 8
+test$Item_Type[test$Item_Type == "Hard Drinks"] = 8
+
+train$Item_Type[train$Item_Type == "Health and Hygiene"] = 9
+test$Item_Type[test$Item_Type == "Health and Hygiene"] = 9
+
+train$Item_Type[train$Item_Type == "Household"] = 10
+test$Item_Type[test$Item_Type == "Household"] = 10
+
+train$Item_Type[train$Item_Type == "Meat"] = 11
+test$Item_Type[test$Item_Type == "Meat"] = 11
+
+train$Item_Type[train$Item_Type == "Others"] = 12
+test$Item_Type[test$Item_Type == "Others"] = 12
+
+train$Item_Type[train$Item_Type == "Seafood"] = 13
+test$Item_Type[test$Item_Type == "Seafood"] = 13
+
+train$Item_Type[train$Item_Type == "Snack Foods"] = 14
+test$Item_Type[test$Item_Type == "Snack Foods"] = 14
+
+train$Item_Type[train$Item_Type == "Soft Drinks"] = 15
+test$Item_Type[test$Item_Type == "Soft Drinks"] = 15
+
+train$Item_Type[train$Item_Type == "Starchy Foods"] = 16
+test$Item_Type[test$Item_Type == "Starchy Foods"] = 16
+
+train$Item_Type = as.integer(train$Item_Type)
+test$Item_Type = as.integer(test$Item_Type)
+
+table(train$Item_Type)
+table(test$Item_Type)
+
+train$Item_MRP = as.integer(train$Item_MRP)
+test$Item_MRP = as.integer(test$Item_MRP)
+
+train$Outlet_Establishment_Year = as.integer(train$Outlet_Establishment_Year)
+test$Outlet_Establishment_Year = as.integer(test$Outlet_Establishment_Year)
+
+train$Outlet_Size[train$Outlet_Size == "Small"] = 1
+test$Outlet_Size[test$Outlet_Size == "Small"] = 1
+
+train$Outlet_Size[train$Outlet_Size == "Medium"] = 2
+test$Outlet_Size[test$Outlet_Size == "Medium"] = 2
+
+train$Outlet_Size[train$Outlet_Size == "High"] = 3
+test$Outlet_Size[test$Outlet_Size == "High"] = 3
+
+train$Outlet_Size = as.integer(train$Outlet_Size)
+test$Outlet_Size = as.integer(test$Outlet_Size)
+
+table(train$Outlet_Size)
+table(test$Outlet_Size)
+
+train$Outlet_Location_Type[train$Outlet_Location_Type == "Tier 1"] = 1
+test$Outlet_Location_Type[test$Outlet_Location_Type == "Tier 1"] = 1
+
+train$Outlet_Location_Type[train$Outlet_Location_Type == "Tier 2"] = 2
+test$Outlet_Location_Type[test$Outlet_Location_Type == "Tier 2"] = 2
+
+train$Outlet_Location_Type[train$Outlet_Location_Type == "Tier 3"] = 3
+test$Outlet_Location_Type[test$Outlet_Location_Type == "Tier 3"] = 3
+
+train$Outlet_Location_Type = as.integer(train$Outlet_Location_Type)
+test$Outlet_Location_Type = as.integer(test$Outlet_Location_Type)
+
+table(train$Outlet_Location_Type)
+table(test$Outlet_Location_Type)
+
+train$Outlet_Type[train$Outlet_Type == "Grocery Store"] = 1
+test$Outlet_Type[test$Outlet_Type == "Grocery Store"] = 1
+
+train$Outlet_Type[train$Outlet_Type == "Supermarket Type1"] = 2
+test$Outlet_Type[test$Outlet_Type == "Supermarket Type1"] = 2
+
+train$Outlet_Type[train$Outlet_Type == "Supermarket Type2"] = 3
+test$Outlet_Type[test$Outlet_Type == "Supermarket Type2"] = 3
+
+train$Outlet_Type[train$Outlet_Type == "Supermarket Type3"] = 4
+test$Outlet_Type[test$Outlet_Type == "Supermarket Type3"] = 4
+
+train$Outlet_Type = as.integer(train$Outlet_Type)
+test$Outlet_Type = as.integer(test$Outlet_Type)
+
+table(train$Outlet_Type)
+table(test$Outlet_Type)
+
+train$Item_Weight = as.integer(train$Item_Weight)
+test$Item_Weight = as.integer(test$Item_Weight)
+
+train$Item_mean_sales = as.integer(train$Item_mean_sales)
+test$Item_mean_sales = as.integer(test$Item_mean_sales)
+
+train$Outlet_Mean = as.integer(train$Outlet_Mean)
+test$Outlet_Mean = as.integer(test$Outlet_Mean)
+
